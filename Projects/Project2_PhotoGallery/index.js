c5b9cefd23cf2c0e5ca15ef38e5cbcb64d7318c5
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-04-13 13:40:01 +0530

    Add active class to panels

--- Projects/Project2_PhotoGallery/index.js
+++ Projects/Project2_PhotoGallery/index.js
@@ -1,5 +1,18 @@
-var panel1 = document.getElementById("panel-1");
-var panel2 = document.getElementById("panel-2");
-var panel3 = document.getElementById("panel-3");
-var panel4 = document.getElementById("panel-4");
-var panel5 = document.getElementById("panel-5");
+let panel1 = document.getElementById("panel-1");
+let panel2 = document.getElementById("panel-2");
+let panel3 = document.getElementById("panel-3");
+let panel4 = document.getElementById("panel-4");
+let panel5 = document.getElementById("panel-5");
+
+function resetActivePanel() {
+    panel1.classList.remove('active')
+    panel2.classList.remove('active')
+    panel3.classList.remove('active')
+    panel4.classList.remove('active')
+    panel5.classList.remove('active')
+}
+
+function selectPanel(panelId) {
+    resetActivePanel()
+    document.getElementById(panelId).classList.add('active')
+}
