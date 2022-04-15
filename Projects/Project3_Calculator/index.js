b03ced1d1497a293cc083b452db8924870a2fc78
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-04-16 00:39:08 +0530

    Add replace condition when two operators are present at same time

--- Projects/Project3_Calculator/index.js
+++ Projects/Project3_Calculator/index.js
@@ -17,11 +17,15 @@ function buttonClicked(btnTxt) {
             }
         }
     } else {
-        displayArea.innerText = displayArea.innerText + btnTxt
-        /*this line will display the new number along with the previous number 
-        i.e.if we press 4 and then press 5 it will show in display part as 45.
-        and the values are considered as String here so concatination is done here.
-        */ 
+        if(isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator((btnTxt))){
+            //  replace the operator
+          } else {
+              displayArea.innerText = displayArea.innerText + btnTxt
+              /*this line will display the new number along with the previous number 
+              i.e.if we press 4 and then press 5 it will show in display part as 45.
+              and the values are considered as String here so concatination is done here.
+              */ 
+          }
     }
     
 }
