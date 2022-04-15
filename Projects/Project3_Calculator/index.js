6347750495b782dab9f48cdacad8cd5af0d945ca
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-04-16 00:23:31 +0530

    Add condition to overcome displaying 0 in the first place

--- Projects/Project3_Calculator/index.js
+++ Projects/Project3_Calculator/index.js
@@ -1,9 +1,14 @@
 var displayArea = document.getElementById('display')
 
-function buttonClicked(digitOrOperator) {
-    displayArea.innerText = displayArea.innerText + digitOrOperator
-    /*this line will display the new number along with the previous number 
-    i.e.if we press 4 and then press 5 it will show in display part as 45.
-    and the values are considered as String here so concatination is done here.
-    */
+function buttonClicked(btnTxt) {
+    if (displayArea.innerText === '0') {
+        displayArea.innerText = btnTxt
+    } else {
+        displayArea.innerText = displayArea.innerText + btnTxt
+        /*this line will display the new number along with the previous number 
+        i.e.if we press 4 and then press 5 it will show in display part as 45.
+        and the values are considered as String here so concatination is done here.
+        */ 
+    }
+    
 }
