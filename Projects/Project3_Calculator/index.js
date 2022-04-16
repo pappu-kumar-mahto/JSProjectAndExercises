a30fbc72f4f2156f43cd50060e6e2a674ed49f44
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-04-16 17:32:18 +0530

    Complete Calculator Project code

--- Projects/Project3_Calculator/index.js
+++ Projects/Project3_Calculator/index.js
@@ -1,31 +1,55 @@
-var displayArea = document.getElementById('display')
+var displayArea = document.getElementById("display");
 
-function isOperator(btnTxt){
-    if(btnTxt == '+' || btnTxt == '-' || btnTxt == '*' || btnTxt == '/' || btnTxt == '%')
-      return true
-    else 
-      return false
-  }
+function isOperator(btnTxt) {
+  if (
+    btnTxt == "+" ||
+    btnTxt == "-" ||
+    btnTxt == "*" ||
+    btnTxt == "/" ||
+    btnTxt == "%"
+  )
+    return true;
+  else return false;
+}
 
 function buttonClicked(btnTxt) {
-    if (displayArea.innerText === '0') { //to stop displaying 0 in the first place
-        if (btnTxt != '0' && btnTxt != '00') { //if display has already 0 don't add further 0
-            if (btnTxt == '.' || isOperator(btnTxt)) { // if btn clicked is '.' or and operator then show . or operator along with zero
-                displayArea.innerText = '0' + btnTxt
-            } else {
-                displayArea.innerText = btnTxt
-            }
+  if (btnTxt != "Clr" && btnTxt != "Del" && btnTxt != "=") {
+    if (displayArea.innerText === "0") {
+      //to stop displaying 0 in the first place
+      if (btnTxt != "0" && btnTxt != "00") {
+        //if display has already 0 don't add further 0
+        if (btnTxt == "." || isOperator(btnTxt)) {
+          // if btn clicked is '.' or and operator then show . or operator along with zero
+          displayArea.innerText = "0" + btnTxt;
+        } else {
+          displayArea.innerText = btnTxt;
         }
+      }
     } else {
-        if(isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator((btnTxt))){
-            //  replace the operator
-          } else {
-              displayArea.innerText = displayArea.innerText + btnTxt
-              /*this line will display the new number along with the previous number 
-              i.e.if we press 4 and then press 5 it will show in display part as 45.
-              and the values are considered as String here so concatination is done here.
-              */ 
-          }
+      if (
+        isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
+        isOperator(btnTxt)
+      ) {
+        displayArea.innerText = displayArea.innerText.slice(0, -1) + btnTxt;
+      } else {
+        displayArea.innerText = displayArea.innerText + btnTxt;
+        /*this line will display the new number along with the previous number 
+                i.e.if we press 4 and then press 5 it will show in display part as 45.
+                and the values are considered as String here so concatination is done here.
+                */
+      }
     }
-    
+  } else {
+    if (btnTxt === "Clr") {
+      displayArea.innerText = "0";
+    } else if (btnTxt === "Del") {
+      displayArea.innerText = displayArea.innerText.slice(0, -1);
+    } else if (btnTxt === "=") {
+      try {
+        displayArea.innerText = eval(displayArea.innerText);
+      } catch (error) {
+        displayArea.innerText = 0;
+      }
+    }
+  }
 }
