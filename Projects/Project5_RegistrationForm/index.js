e9226442dcd846cb679bd7dc4b65fd290d65969e
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-04-23 11:09:56 +0530

    Add JavaScript file code with initial validation function

---
+++ Projects/Project5_RegistrationForm/index.js
@@ -0,0 +1,11 @@
+function validation() {
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let email = document.getElementById("email").value;
+    let phoneNumber = document.getElementById("phoneNumber").value;
+    let state = document.getElementById("state").value;
+    let zipCode = document.getElementById("zipCode").value;
+  
+    console.log("validation works fine");
+  }
+  
