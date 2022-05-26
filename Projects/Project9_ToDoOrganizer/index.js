1b14656d787c55a8c52fa2cc3c5dd610bb0028f5
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-05-26 13:50:04 +0530

    Project: To-Do Organizer ; boxShadow added to To-Do card and deadline added.

--- Projects/Project9_ToDoOrganizer/index.js
+++ Projects/Project9_ToDoOrganizer/index.js
@@ -1,55 +1,68 @@
 /*************************DOM NODE SELECTION***********************/
 
-const toDoBlock = document.getElementById('toDoBlock')
-const addNewTask = document.getElementById('addNewTask')
-const modalInputID = document.getElementById('modalInputID')
-const selectPriority = document.getElementById('selectPriority')
+const toDoBlock = document.getElementById("toDoBlock");
+const addNewTask = document.getElementById("addNewTask");
+const toDoInput = document.getElementById("toDoInput");
+const selectPriority = document.getElementById("selectPriority");
+const toDoDeadLine = document.getElementById("toDoDeadLine");
 
 /**********************CREATE A NEW TO-DO TASK CARD***************/
 
-const createToDoCard = (newToDo, priority) => {
-    let toDoCardDiv = document.createElement('div')
-    let cardHeaderDiv = document.createElement('div')
-    let cardHeaderSpan = document.createElement('span')
-    let cardBodyDiv = document.createElement('div')
-    let cardTitleH5 = document.createElement('h5')
-    let delButton = document.createElement('a')
+const createToDoCard = (newToDo, priority, deadline) => {
+  const toDoCardDiv = document.createElement("div");
+  const cardHeaderDiv = document.createElement("div");
+  const cardHeaderSpan = document.createElement("span");
+  const cardHeaderDeadline = document.createElement("span");
+  const cardBodyDiv = document.createElement("div");
+  const cardTitleH5 = document.createElement("h5");
+  const delButton = document.createElement("a");
 
-    toDoCardDiv.classList = 'card'
-    cardHeaderDiv.classList = 'card-header'
-    cardHeaderSpan.classList = 'badge rounded-pill bg-danger'
-    cardBodyDiv.classList = 'card-body'
-    cardTitleH5.classList = 'card-title'
-    delButton.classList = 'btn btn-danger btn-xs'
-    toDoCardDiv.id = "cardID"
-    delButton.id = 'deleteToDo'
+  toDoCardDiv.classList = "card toDoCard";
+  cardHeaderDiv.classList = "card-header";
+  cardBodyDiv.classList = "card-body";
+  cardTitleH5.classList = "card-title";
+  delButton.classList = "btn btn-danger btn-sm";
+  delButton.id = "deleteToDo";
 
-    cardTitleH5.innerText = newToDo
-    cardHeaderSpan.innerText = priority
-    delButton.innerText = 'Delete'
+  switch (priority) {
+    case "High":
+      cardHeaderSpan.classList = "badge rounded-pill bg-danger";
+      break;
+    case "Medium":
+      cardHeaderSpan.classList = "badge rounded-pill bg-warning text-dark";
+      break;
+    case "Low":
+      cardHeaderSpan.classList = "badge rounded-pill bg-info text-dark";
+      break;
+  }
 
-    toDoBlock.appendChild(toDoCardDiv)
-    toDoCardDiv.appendChild(cardHeaderDiv)
-    cardHeaderDiv.appendChild(cardHeaderSpan)
-    toDoCardDiv.appendChild(cardBodyDiv)
-    cardBodyDiv.appendChild(cardTitleH5)
-    cardBodyDiv.appendChild(delButton)
-}
+  cardTitleH5.innerText = `${newToDo[0].toUpperCase()}${newToDo.slice(1)}`;
+  cardHeaderSpan.innerText = `${priority} Priority`;
+  cardHeaderDeadline.innerText = deadline;
+  delButton.innerText = "Delete";
+
+  toDoBlock.appendChild(toDoCardDiv);
+  toDoCardDiv.appendChild(cardHeaderDiv);
+  cardHeaderDiv.appendChild(cardHeaderSpan);
+  cardHeaderDiv.appendChild(cardHeaderDeadline);
+  toDoCardDiv.appendChild(cardBodyDiv);
+  cardBodyDiv.appendChild(cardTitleH5);
+  cardBodyDiv.appendChild(delButton);
+};
 
 /******************ADD NEW TASK TO "To-Do" SECTION*********************/
 
-addNewTask.addEventListener('click', () => {
-    if (!modalInputID.value || selectPriority.value === "Select Priority") {
-        alert('Please Enter a TO-DO and Select Priority')
-        return
-    }
-    createToDoCard(modalInputID.value, selectPriority.value)
-    const cardBlock = document.getElementById('cardID')
-    console.log(cardBlock)
-    const deleteToDo = document.getElementById("deleteToDo")
-    deleteToDo.addEventListener("click", () => {
-        // cardBlock.style.display = "none"
-        toDoBlock.removeChild(cardBlock)
-    })
-})
+addNewTask.addEventListener("click", () => {
+  if (
+    !toDoInput.value ||
+    selectPriority.value === "Select Priority" ||
+    !toDoDeadLine.value
+  ) {
+    alert("Please Enter a TO-DO, Select Priority and Enter a valid DateTime");
+    return;
+  }
 
+  const Datevalue = moment(toDoDeadLine.value).format("YYYY/MM/DD h:mm A");
+
+  createToDoCard(toDoInput.value, selectPriority.value, Datevalue);
+});
