95d72c5d28ec8d17167717e23407905e48ce16f9
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-05-31 12:33:53 +0530

    Project: Shopping App; continued: smooth scrolling added, spinner added and onload event added.

--- Projects/Project10_ShoppingApp/js/index.js
+++ Projects/Project10_ShoppingApp/js/index.js
@@ -1,6 +1,8 @@
+const productSpinner = document.getElementById('products-spinner')
+
 const fetchProducts = async () => {
     const res = await fetch('https://fakestoreapi.com/products')
     const data = await res.json()
-    // console.log(data)
+    productSpinner.style.display = 'none'
     data.forEach(product => createProductDiv(product))  
 }
