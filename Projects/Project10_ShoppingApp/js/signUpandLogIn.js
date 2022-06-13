e437288690780cfd2d81ce4e50345318c801dca5
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-06-13 21:20:14 +0530

    Project: Shopping App; SignUp modal created and authentication done.

--- Projects/Project10_ShoppingApp/js/signUpandLogIn.js
+++ Projects/Project10_ShoppingApp/js/signUpandLogIn.js
@@ -4,10 +4,6 @@ let passwordStoredInDB = "";
 let emailStoredInDB = ""
 
 let singnUpPass = document.getElementById("signUpPassword");
-let rightSignUpPass = document.getElementById("rightSignUpPassword");
-let wrongSignUpPass = document.getElementById("wrongSignUpPassword");
-// let SignUpSaved = document.getElementById("SignUpSaved");
-// let SignUpNotSaved = document.getElementById("SignUpNotSaved");
 
 let passMeterBar = document.getElementById("passMeterBar")
 let passMeterLebel = document.getElementById("passMeterLebel")
@@ -131,55 +127,6 @@ const logIn = () => {
   }
 };
 
-let passwordStrengthCheck = () => {
-  passMeterBarInner.style.marginBottom = "0px";
-  let strongPassword = new RegExp(
-    "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-za-z0-9](?=.{8,}))"
-  );
-  let mediumPassword = new RegExp(
-    "((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-za-z0-9])(?=.{5,})|(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-za-z0-9])(?=.{8,}))"
-  );
-
-  if (singnUpPass.value === "") {
-    passLebel.style.display = "none"
-    passMeterLebel.style.display = "none"
-    passMeterBarInner.style.display = "none"
-  } else {
-    
-    passMeterLebel.style.display = "block"
-    if (strongPassword.test(singnUpPass.value)) {
-      
-      passLebel.style.display = "block"
-      passLebel.innerText = "Strong"
-      passLebel.style.color = "#26de81"
-      
-      passMeterBarInner.style.display = "block"
-      passMeterBarInner.style.width = "80%"
-      passMeterBarInner.style.background = "#26de81"
-      
-    } else if (mediumPassword.test(singnUpPass.value)) {
-      
-      passLebel.style.display = "block"
-      passLebel.innerText = "Modrate"
-      passLebel.style.color = "#fd9644"
-      
-      passMeterBarInner.style.display = "block"
-      passMeterBarInner.style.width = "66%"
-      passMeterBarInner.style.background = "#fd9644"
-      
-    } else {
-      
-      passLebel.style.display = "block"
-      passLebel.innerText = "Weak"
-      passLebel.style.color = "#fc5c65"
-      
-      passMeterBarInner.style.display = "block"
-      passMeterBarInner.style.width = "33%"
-      passMeterBarInner.style.background = "#fc5c65"
-    }
-  }
-}
-
 /*************************************************************************************************************************************************************/
 /* Form Validation part*/
 let firstNameValid = document.getElementById("firstNameValid")
@@ -191,46 +138,32 @@ let lastNameInvalid = document.getElementById("lastNameInvalid")
 let emailValid = document.getElementById("emailValid")
 let emailInvalid = document.getElementById("emailInvalid")
 
-let phoneNumberValid = document.getElementById("phoneNumberValid")
-let phoneNumberInvalid = document.getElementById("phoneNumberInvalid")
-
-let stateValid = document.getElementById("stateValid")
-let stateInvalid = document.getElementById("stateInvalid")
-
-let zipCodeValid = document.getElementById("zipCodeValid")
-let zipCodInvalid = document.getElementById("zipCodeInvalid")
-
-let tncInvalid = document.getElementById("tncInvalid")
+let rightSignUpPass = document.getElementById("rightSignUpPass");
+let wrongSignUpPass = document.getElementById("wrongSignUpPass");
 
 let firstName = document.getElementById("firstName")
 let lastName = document.getElementById("lastName")
 let email = document.getElementById("email")
-let phoneNumber = document.getElementById("phoneNumber")
-let state = document.getElementById("state")
-let zipCode = document.getElementById("zipCode")
-let tncCheckBox = document.getElementById("tncCheckBox")
-
-let submissionMsg = document.getElementById("submissionMsg")
 
 let validate = (response) => {
   switch (response) {
     case "firstName": {
       if (firstName.value === "") {
-        firstNameInvalid.style.display = "block"
-        firstNameValid.style.display = "none"
+        firstNameInvalid.style.display = "block";
+        firstNameValid.style.display = "none";
       } else {
-        firstNameValid.style.display = "block"
-        firstNameInvalid.style.display = "none"
+        firstNameValid.style.display = "block";
+        firstNameInvalid.style.display = "none";
       }
       break;
     }
     case "lastName": {
       if (lastName.value === "") {
-        lastNameInvalid.style.display = "block"
-        lastNameValid.style.display = "none"
+        lastNameInvalid.style.display = "block";
+        lastNameValid.style.display = "none";
       } else {
-        lastNameValid.style.display = "block"
-        lastNameInvalid.style.display = "none"
+        lastNameValid.style.display = "block";
+        lastNameInvalid.style.display = "none";
       }
       break;
     }
@@ -250,46 +183,13 @@ let validate = (response) => {
       }
       break;
     }
-    case "phoneNumber": {
-      if (
-        phoneNumber.value === "" ||
-        phoneNumber.value.length != 10 ||
-        Number(phoneNumber.value[0]) < 6
-      ) {
-        // !phoneNumber.match(/^[6-9]\d{9}$/g)
-        phoneNumberInvalid.style.display = "block"
-        phoneNumberValid.style.display = "none"
+    case "singnUpPass": {
+      if (singnUpPass.value === "" || singnUpPass.value.length < 8) {
+        rightSignUpPass.style.display = "none";
+        wrongSignUpPass.style.display = "block";
       } else {
-        phoneNumberValid.style.display = "block"
-        phoneNumberInvalid.style.display = "none"
-      }
-      break;
-    }
-    case "state": {
-      if (state.value === "" || state.value === "...") {
-        stateInvalid.style.display = "block"
-        stateValid.style.display = "none"
-      } else {
-        stateValid.style.display = "block"
-        stateInvalid.style.display = "none"
-      }
-      break;
-    }
-    case "zipCode": {
-      if (zipCode.value === "" || zipCode.value.length != 6) {
-        zipCodInvalid.style.display = "block"
-        zipCodeValid.style.display = "none"
-      } else {
-        zipCodeValid.style.display = "block"
-        zipCodInvalid.style.display = "none"
-      }
-      break;
-    }
-    case "tncCheckBox": {
-      if (!tncCheckBox.checked) {
-        tncInvalid.style.display = "block"
-      } else {
-        tncInvalid.style.display = "none"
+        rightSignUpPass.style.display = "block";
+        wrongSignUpPass.style.display = "none";
       }
       break;
     }
@@ -300,21 +200,14 @@ let validateAll = () => {
   validate("firstName")
   validate("lastName")
   validate("email")
-  validate("phoneNumber")
-  validate("state")
-  validate("zipCode")
-  validate("tncCheckBox")
-
+  validate("singnUpPass")
   if (
     firstNameInvalid.style.display === "none" &&
     lastNameInvalid.style.display === "none" &&
     emailInvalid.style.display === "none" &&
-    phoneNumberInvalid.style.display === "none" &&
-    stateInvalid.style.display === "none" &&
-    zipCodeInvalid.style.display === "none" &&
-    tncInvalid.style.display === "none"
+    wrongSignUpPass.style.display === "none"
   ) {
-    submissionMsg.innerHTML = "Registration Successfull."
+    
   }
 };
 
@@ -325,15 +218,7 @@ let refresh = () => {
   lastNameInvalid.style.display = "none"
   emailValid.style.display = "none"
   emailInvalid.style.display = "none"
-  passMeterBarInner.style.display = "none"
-  passMeterLebel.style.display = "none"
-  phoneNumberValid.style.display = "none"
-  phoneNumberInvalid.style.display = "none"
-  stateValid.style.display = "none"
-  stateInvalid.style.display = "none"
-  zipCodeValid.style.display = "none"
-  zipCodeInvalid.style.display = "none"
-  tncInvalid.style.display = "none"
+  
   logInMatch.style.display = "none"
   logInNotMatch.style.display = "none"
 
@@ -341,11 +226,7 @@ let refresh = () => {
   lastName.value = ""
   email.value = ""
   singnUpPass.value = ""
-  phoneNumber.value = ""
-  state.value = ""
-  zipCode.value = ""
-  tncCheckBox.checked = false
-  submissionMsg.innerHTML = ""
+ 
   passwordStoredInDB = ""
   emailStoredInDB = ""
   logInPass.value = ""
@@ -362,12 +243,7 @@ let homePage = () => {
   logInPass.value = ""
   logInEmail.value = ""
 }
-let signupForm = () => {
-  document.getElementById('signupForm').style.display = "block",
-  document.getElementById('signupForm').style.backgroundColor = "azure",
-  document.getElementById('signupForm').style.borderRadius = "5px"
-  document.getElementById('loginForm').style.display = "none"
-}
+
 let loginForm = () => {
   document.getElementById('loginForm').style.display = "block",
   document.getElementById('loginForm').style.backgroundColor = " aliceblue",
